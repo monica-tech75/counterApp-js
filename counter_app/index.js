b3d388f9f21a75fcb60ab267ff8e1f6740ef8ae4
Monica Pardo <moonyruna3@gmail.com>
2024-05-23 18:30:20 +0200

    Change background color in secondary botton when clicked and create new files

--- counter_app/index.js
+++ counter_app/index.js
@@ -3,30 +3,25 @@
 // Increment the count variable by one when the button is clicked and log it out
 // change the count element in the HTML to reflect the new count
 let count = 0;
-const counter = document.getElementById('counter');
-const lastCounts = document.getElementById('lastCounts');
-//const welcomeEl = document.getElementById('welcome');
-
-// let firstName = prompt('What is your name?');
-// let greetings = `Welcome ${firstName}, nice to see you again!`;
-// welcomeEl.innerText = greetings;
-// welcomeEl.innerText += " 💕";
-
+const counter = document.getElementById("counter");
+const lastCounts = document.getElementById("lastCounts");
+const welcomeEl = document.getElementById("welcome");
+const buttonSecondaryEl = document.getElementById("btn-secondary");
+let firstName = prompt("What is your name?");
+let greetings = `Welcome ${firstName}, nice to see you again!`;
+welcomeEl.innerText = greetings;
+welcomeEl.innerText += " 💕";
 
 function increment() {
-  count+= 1;
+  count += 1;
   counter.textContent = count;
-};
+  buttonSecondaryEl.style.backgroundColor = "rgb(62, 167, 132)";
+}
 
 function save() {
-  let countStr = '  ' + count + ' - ';
+  let countStr = "  " + count + " - ";
   lastCounts.textContent += countStr;
   counter.textContent = 0;
   count = 0;
-};
-
-
-
-
-
-
+  buttonSecondaryEl.style.backgroundColor = "blue";
+}
